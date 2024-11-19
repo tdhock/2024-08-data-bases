b37d138f9a3b4954d62ea941cbc0d8c24f2784d2
Toby Dylan Hocking <toby.hocking@r-project.org>
2024-11-19 12:23:00 -0500

    sql contraintes

--- natural-left-join.sql
+++ natural-left-join.sql
@@ -1,24 +1,57 @@
 CREATE TABLE Etudiant (
 matricule int NOT NULL,
-nom varchar(20) NOT NULL
+nom varchar(20) NOT NULL,
+CONSTRAINT matricule_clef_primaire PRIMARY KEY (matricule)
 )
 
-INSERT INTO Etudiant (matricule, nom)
+INSERT INTO Étudiant_uds_😎 (matricule, nom)
 VALUES
 (150, 'Paul'),
 (354, 'Eliane'),
 (870, 'Mohamed'),
 (132, 'Sergei');
 
-DROP TABLE Resultat;
+UPDATE Etudiant SET matricule=99 WHERE matricule=132;
+SELECT * FROM Resultat_avec_Contrainte
 
-CREATE TABLE Resultat (
-matricule int NOT NULL,
-note int NOT NULL,
-sigle varchar(6) NOT NULL
-)
+ALTER TABLE Resultat_avec_contrainte
+ADD 
+CONSTRAINT matricule_reference_etudiant
+FOREIGN KEY (matricule) REFERENCES Etudiant
+ON UPDATE CASCADE;
+
+DROP CONSTRAINT matricule_reference_etudiant;
 
 INSERT INTO Resultat 
+(matricule, note, sigle) VALUES
+(99,100,'IFT159');
+
+SELECT * FROM Resultat_avec_contrainte;
+ALTER TABLE Resultat_avec_contrainte
+RENAME note_contrainte to note;
+UPDATE Resultat_avec_contrainte
+SET note_contrainte = note*0.9;
+DROP TABLE Resultat_avec_contrainte;
+
+CREATE DOMAIN Note
+SMALLINT
+CHECK (VALUE BETWEEN 0 AND 100);
+
+ALTER TABLE Resultat_avec_contrainte
+ALTER note_contrainte SET NOT NULL;
+ALTER TABLE Resultat_avec_contrainte
+DROP note;
+
+CREATE TABLE Resultat_avec_contrainte (
+matricule int NOT NULL,
+note Note NOT NULL,
+sigle varchar(6) NOT NULL,
+CONSTRAINT matricule_reference_etudiant
+FOREIGN KEY (matricule) REFERENCES Etudiant,
+CONSTRAINT matricule_sigle_unique UNIQUE (matricule,sigle)
+)
+
+INSERT INTO Resultat_avec_contrainte 
 (matricule, note, sigle) VALUES
 (150,80,'IFT187'),
 (354,78,'IFT187'),
@@ -27,6 +60,15 @@ INSERT INTO Resultat
 (132,90,'IMN117'),
 (132,45,'IFT187'),
 (354,52,'IFT159');
+
+SELECT *
+DELETE
+FROM Resultat_avec_contrainte
+WHERE SUBSTR(sigle,1,3)='IFT';
+
+SELECT * FROM Resultat_avec_contrainte;
+
+UPDATE Resultat_avec_Contrainte SET note = note * 1.1;
 
 SELECT substring(sigle,1,3) AS depart FROM Resultat;
 
